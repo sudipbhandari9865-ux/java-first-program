8bf8797e47236593c06a251e45e8f276a88724ab
unknown <sudipbhandari9865-ux>
2026-03-12 09:54:58 +0545

    test java file added

---
+++ test.java
@@ -0,0 +1,19 @@
+class Test{
+	public static void main(String[] args){
+
+	System.out.print("Hello world");
+
+}
+}
+
+/*------ Excution process of java program -----
+
+1. compilation (jvac) ... source code--> byte code
+ a. synatx analysis
+ b. semantic analysis
+command(to change in byte code)--> javac test.java
+
+2. Run(jvm) .... jvm --> machine code --->output
+command(for run)--> java Test 
+
+*/
