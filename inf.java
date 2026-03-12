c90964980edfe4f094d72eb3f7fdd1b71c69b144
unknown <sudipbhandari9865-ux>
2026-03-12 09:32:44 +0545

    info and test java file added

---
+++ inf.java
@@ -0,0 +1,8 @@
+class info{
+	public static void main(String[] args){
+
+	System.out.println("my information");
+        System.out.println("my campus kmc");
+        System.out.println("my campus addressdhangadhi");
+}
+}
